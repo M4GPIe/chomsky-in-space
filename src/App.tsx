1ba41637db3581530f84f4f0b936d51a004b2964
M4GPIe <delossantossanchismanuel@gmail.com>
2024-10-05 15:15:26 +0200

    feat: deleted default project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material"
+import PlanetScene from "./views/components/PlanetScene"
+
+export const App = () => {
+  return (
+    <Box sx={{width: '50%'}}>
+      <PlanetScene/>
+    </Box>
+  )
+}
